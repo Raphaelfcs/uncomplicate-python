53da8c4f395b5bae0956b96efa9341403234f02a
Raphael <raphael@Raphaels-MacBook-Pro.local>
2022-05-29 00:44:14 -0300

    criando comentários, importando os(falar com SO assim podemos cpletar dados do sistema) usando Dunder para metadados usando variável, declarando valor padrão de variável, usando condições para trocar o valor da variável

--- dia-01/hello.py
+++ dia-01/hello.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3.7
+"""
+Hello Word Mult Linguas.
 
-#este programa imprime a mensagem Hello Word
-print("Hello, World!!!") # teste-ignore😆
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+    export LANG=pt_BR essa é do SO nativo
+    ou 
+    export LANGS_DEMO=pt_BR essa é criada com export
+
+Execução:
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+
+__version__ = "0.0.1"
+__author__ = "Raphael Ferreira"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANGS_DEMO", "en_US")[:5]
+
+msg = "Hello, World!!!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!!" 
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo"
+elif current_language == "es_SP":
+    msg = "Hola, Mondo"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde"
+
+print(msg)
